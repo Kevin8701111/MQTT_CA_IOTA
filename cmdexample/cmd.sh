ff9dd429f174a33222692323f436d713cab85f70
kevin8701111 <kevin8701111@gmail.com>
2022-02-28 17:12:53 +0800

    add client cmd

--- cmdexample/cmd.sh
+++ cmdexample/cmd.sh
@@ -1,1 +1,3 @@
+
 mosquitto_sub -h 192.168.0.XXX -p 8883 -t "kk/123" --cafile ca.crt --cert server.crt --key server.key -d --tls-version tlsv1.2
+mosquitto_pub -h 192.168.0.XXX -p 8883 -t "kk/123" --cafile ca.crt --cert client.crt --key client.key -d --tls-version tlsv1.2 -m "efefefefd"
